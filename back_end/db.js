9f86752f691da21b522c37192909be9ec830c874
kuda2012 <kuda2012@gmail.com>
2023-06-03 16:11:53 -0500

    remove ssl

--- back_end/db.js
+++ back_end/db.js
@@ -3,6 +3,7 @@ const { DB_URI } = require("./config");
 
 const db = new Client({
   connectionString: DB_URI,
+  ssl: process.env.DB_URI ? { rejectUnauthorized: false } : false,
 });
 
 db.connect();
